0348a3b62f80f6f35f1cdd112e19cf1bdcb6e26e
qwe661234 <qwe661234@gmail.com>
2022-10-09 01:23:52 +0800

    Modify create_default_list to create reversely

--- HW1-LeetCode-203-RISCV/LeetCode-203-data-1.s
+++ HW1-LeetCode-203-RISCV/LeetCode-203-data-1.s
@@ -2,7 +2,7 @@
 length:
     .word 7
 array: 
-    .word 1, 2, 6, 3, 4, 5, 6
+    .word 6, 5, 4, 3, 6, 2, 1
 target:
     .word 6 
 answer:
@@ -30,41 +30,29 @@ exit:
 # ------ create_default_list ------
 create_default_list:
     addi sp, sp, 8
+    li s0, 0x0fff0000 # address of list head
+    sw ra, 0(sp)      # store return address
+    sw s0, 4(sp)      # store the address of list head
     li s2, 0          # i = 0
     la t1, array      # load array
     la t0, length     # load length
     lw t0, 0(t0)      
-    bne s2, t0, create
+    bne s2, t0, loop
     jr ra
-create:
-    li s0, 0x0fff0000 # address of list head
-    sw ra, 0(sp)      # store return address
-    sw s0, 4(sp)      # store the address of list head
-    jal ra, malloc    # get memory for the next node
-    bne a0, x0, exit_error  
-    lw s1, 0(t1)      # load array[i]
-    addi t1, t1, 4    # i++
-    sw s1, 0(s0)      # node->value = array[i]
-    sw x0, 4(s0)      # node->next = NULL
-    mv t2, s0         # prev = node
-    addi s2, s2, 1    
-    addi s0, s0, 8    
-    beq s2, t0, ret   # check end condition in for loop
 loop:   
     jal ra, malloc    # get memory for the next node
     bne a0, x0, exit_error  
     lw s1, 0(t1)      # load array[i]
     addi t1, t1, 4    # i++
     sw s1, 0(s0)      # node->value = array[i]
-    sw x0, 4(s0)      # node->next = NULL
-    sw s0, 4(t2)      # prev->next = node
+    sw t2, 4(s0)      # prev->next = node
     mv t2, s0         # prev = node
     addi s2, s2, 1    
     addi s0, s0, 8    
     bne s2, t0, loop  # check end condition in for loop
 ret:
+    addi s1, s0, -8
     lw ra, 0(sp)      # load return address
-    lw s1, 4(sp)      # load the address of list head
     addi sp, sp, 8
     jr ra
 # ------ malloc ------
@@ -73,6 +61,28 @@ malloc:     # allocates 8 bytes on the heap, returns pointer to start in a0
     addi a7, x0, 214 # brk systemcall
     ecall
     jr  ra
+# ------ remove_elements ------
+remove_elements:
+    la s0, target
+    lw s0, 0(s0)
+iterate:
+    beq t1, x0, end_iterate 
+    lw t2, 0(t1) # load value store in node 
+    beq t2, s0, remove
+    mv t3, t1    # prev = cur
+    lw t1, 4(t1) # load address store in node->next
+    j iterate
+remove:
+    beq t1, s1, remove_head # if cur == head
+    lw t1, 4(t1) # load cur->next to t0
+    sw t1, 4(t3) # prev->next = cur->next
+    j iterate
+remove_head:
+    lw t1, 4(t1) # load address store in cur->next
+    mv s1, t1 # head = head->next
+    j iterate
+end_iterate:
+    jr ra 
 # ------ print_list ------
 print_list:
     beq t1, x0, end
@@ -92,32 +102,6 @@ exit_error:
     ecall
     addi a7, x0 10 # exit
     ecall   
-# ------ remove_elements ------
-remove_elements:
-    la s0, target
-    lw s0, 0(s0)
-iterate:
-    beq t1, x0, end_iterate 
-    lw a0, 0(t1) # load value store in node 
-    beq a0, s0, remove
-    mv t2, t1    # prev = cur
-    lw t1, 4(t1) # load address store in node->next
-    j iterate
-remove:
-    beq t1, s1, remove_head # if cur == head
-    lw t1, 4(t1) # load cur->next to t0
-    sw t1, 4(t2) # prev->next = cur->next
-    lw a0, 0(t1) # load value store in node
-    beq t1, x0, end_iterate 
-    j iterate
-remove_head:
-    lw t1, 4(t1) # load address store in cur->next
-    lw a0, 0(t1) # load value store in node
-    mv s1, t1
-    beq t1, x0, end_iterate 
-    j iterate
-end_iterate:
-    jr ra 
 # ------ assert_list ------
 assert_list:
     la t0, answer
